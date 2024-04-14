7d9d9aae0c35d7fc544e4661e7cd8d0ee4d77ec7
matheusVitor1 <matheus_vitoralves@hotmail.com>
2024-04-14 15:06:57 -0300

    update do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+
 
 export const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch:'full'},
-  {path: 'home', component: HomeComponent},
+
 ];
 
 @NgModule({
